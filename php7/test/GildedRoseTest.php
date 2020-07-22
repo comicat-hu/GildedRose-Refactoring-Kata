79571dc3f0658cd688ea66d494d2691f55d276e9
comicat-hu <senline39518@gmail.com>
2020-07-22 17:55:02 +0800

    add test cases for php7

--- php7/test/GildedRoseTest.php
+++ php7/test/GildedRoseTest.php
@@ -9,4 +9,109 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase {
         $gildedRose->updateQuality();
         $this->assertEquals("foo", $items[0]->name);
     }
+
+    public function testQualityNeverIsNegative() {
+        $items      = [new Item("foo", 0, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $items[0]->quality);     
+    }
+
+    public function testSulfurasCouldNotBeSold() {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", 10, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(10, $items[0]->sell_in);
+    }
+
+    public function testSulfurasCouldNotDecreaseQuality() {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", 10, 10)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(10, $items[0]->quality);
+    }
+
+    public function testQualityCouldNotBeMoreThanFifty() {
+        $items = [new Item("Aged Brie", 10, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testItemWithDatePassedQualityDecreaseByTwice() {
+        $items = [new Item("foo", -1, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(38, $items[0]->quality);
+    }
+
+    public function testAgedBrieIncreaseQualityWhenItGetsOlder() {
+        $items = [new Item("Aged Brie", 1, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(41, $items[0]->quality);
+    }
+
+    public function testAgedBrieIncreaseByTwoQualityWhenDatePassed() {
+        $items = [new Item("Aged Brie", -1, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(42, $items[0]->quality);
+    }
+
+    public function testAgedBrieIncreaseByTwoQualityWhenDatePassedAndNotMoreThanFifty() {
+        $items = [new Item("Aged Brie", -1, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testBackstagePassesIncreaseQualityByTwoWhenSellinLessThanTen() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(42, $items[0]->quality);
+    }
+
+    public function testBackstagePassesIncreaseQualityByTwoWhenSellinLessThanSix() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 6, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(42, $items[0]->quality);
+    }
+
+    public function testBackstagePassesIncreaseQualityByThreeWhenSellinLessThanFive() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(43, $items[0]->quality);
+    }
+
+    public function testBackstagePassesIncreaseQualityByTwoWhenSellinLessThanSixAndNotMoreThanFifty() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 6, 49)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testBackstagePassesIncreaseQualityByThreeWhenSellinLessThanFiveAndNotMoreThanFifty() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    public function testBackstagePassesQualityDropsToZeroAfterConcert() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $items[0]->quality);
+    }
+
+    public function testBackstagePassesQualityIncreaseQualityByOneWhenDateIsMoreThan10() {
+        $items = [new Item("Backstage passes to a TAFKAL80ETC concert", 11, 40)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(41, $items[0]->quality);
+    }
 }
